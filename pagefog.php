53c37df5850165d61de50bc6a885f462f4cc6f03
Derek Myers <arcticpro@gmail.com>
2016-01-25 23:12:57 -0600

    Initial commit

---
+++ pagefog.php
@@ -0,0 +1,26 @@
+<?php
+
+if ( is_admin() ) {
+	add_filter( 'admin_footer_text', 'pagefog_poweredby_credit', 11 );
+}
+else {
+	add_filter( 'widget_meta_poweredby', 'pagefog_meta_poweredby', 11 );
+	add_action( 'twentyten_credits', array( $this, 'pagefog_poweredby_credit' ) );
+	add_action( 'twentyeleven_credits', array( $this, 'pagefog_poweredby_credit' ) );
+	add_action( 'twentytwelve_credits', array( $this, 'pagefog_poweredby_credit' ) );
+	add_action( 'twentythirteen_credits', array( $this, 'pagefog_poweredby_credit' ) );
+	add_action( 'twentyfourteen_credits', array( $this, 'pagefog_poweredby_credit' ) );
+	add_action( 'twentyfifteen_credits', array( $this, 'pagefog_poweredby_credit' ) );
+}
+
+function pagefog_link($title = 'Pagefog') {
+	return '<a href="http://pagefog.com" target="_blank">'.$title.'</a>';
+}
+
+function pagefog_meta_poweredby() {
+	return pagefog_link();
+}
+
+function pagefog_poweredby_credit() {
+	return pagefog_link('Hosted by Pagefog');
+}
